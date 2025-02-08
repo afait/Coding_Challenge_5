b53460167d5c8ebf7608d4bbe672a7595f61aebe
Annabelle Fait <imannaf8@gmail.com>
2025-02-08 17:08:40 -0500

    Task 1 - Customer Profile

---
+++ cc_5.js
@@ -0,0 +1,8 @@
+// Task 1 - Customer Profile
+
+let customer = {
+    name: "Buzz Lightyear",
+    age: 24,
+    email: "toinfinity&beyond@hotmail.com"
+};
+console.log("Customer Information: ", customer);
