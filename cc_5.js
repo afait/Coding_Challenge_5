1943d49166fe6d98116463626e9631a7e7d39c86
Annabelle Fait <imannaf8@gmail.com>
2025-02-08 22:54:11 -0500

    Task 2 - Object Methods

--- cc_5.js
+++ cc_5.js
@@ -4,5 +4,17 @@ let customer = {
     name: "Buzz Lightyear",
     age: 24,
     email: "toinfinity&beyond@hotmail.com"
-};
-console.log("Customer Information: ", customer);
+}; // create customer with properties name age and email
+console.log("Customer Information: ", customer); // log customer information
+
+// Task 2 - Object Methods
+
+let order = {
+    orderID: 101,
+    totalAmount: 200,
+    status: "Processing", // create an order with an assigned orderID, amount and status
+    displayOrder: function(){ // create the method displayorder
+        console.log(`OrderID:: ${this.orderID}, Amount: ${this.totalAmount}, Status: ${this.status}`);
+    }}; // allows to log the order as one long string with all the details
+
+order.displayOrder(); //console log of teh string of order details
