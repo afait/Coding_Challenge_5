04b35b927c55de19e2f86db79876f9b1ae370061
Annabelle Fait <imannaf8@gmail.com>
2025-02-09 16:53:39 -0500

    Task 3 - Shopping Cart

--- cc_5.js
+++ cc_5.js
@@ -21,7 +21,7 @@ order.displayOrder(); //console log of teh string of order details
 
 // Task 3 - Array Manipulation
 
-let cartItems = ["blaster", "space shoes", "alien repellent"]; // Declared an array as cartItems and created three products to be in the cart
+let cartItems = ["blaster", "space shoes", "alien repellent"]; // Declared an array as cartItems, and created three products to be in the cart
 cartItems.push("helmet"); // adds "helemt" to the cart
 cartItems.pop(); // removes the last item "helment" from the cart
 cartItems.unshift("oxygen"); // adds item "oxygen" to the cart list at the start of the array
