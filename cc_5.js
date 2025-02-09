943ae59fb2378a3a1bb753ffdf7d79355078d2fe
Annabelle Fait <imannaf8@gmail.com>
2025-02-09 00:48:49 -0500

    Task 3 - Array Manipulation

--- cc_5.js
+++ cc_5.js
@@ -18,3 +18,12 @@ let order = {
     }}; // allows to log the order as one long string with all the details
 
 order.displayOrder(); //console log of teh string of order details
+
+// Task 3 - Array Manipulation
+
+let cartItems = ["blaster", "space shoes", "alien repellent"]; // Declared an array as cartItems and created three products to be in the cart
+cartItems.push("helmet"); // adds "helemt" to the cart
+cartItems.pop(); // removes the last item "helment" from the cart
+cartItems.unshift("oxygen"); // adds item "oxygen" to the cart list at the start of the array
+cartItems.shift(); // Removes the first item "oxygen"
+console.log("cartItems: ", cartItems); // Log of the final array
